c042a3659bc452e9ca5df0863d4582fb6aa1ea11
Priscouw <priscouw@gmail.com>
2024-08-07 15:27:19 +0200

    <feat> create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
+import logoHeader from "../../assets/img/logoHeader.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logoHeader} alt="logo kasa" />
+      <div className="navigation">
+        <ul>
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Accueil</li>
+          </NavLink>
+          <NavLink
+            to="/apropos"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>A Propos</li>
+          </NavLink>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
