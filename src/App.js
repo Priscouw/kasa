3e448538f1d92b2fbcdcbf7fe003ec14ef5a0600
Priscouw <priscouw@gmail.com>
2024-08-07 15:28:18 +0200

    <feat> Create router app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Accueil from "./pages/Accueil/Accueil";
+import Apropos from "./pages/Apropos/Apropos";
+import Erreur from "./pages/Erreur/Erreur";
+
 function App() {
   return (
     <div>
-      <h1>Hello react</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/apropos" element={<Apropos />} />
+          <Route path="*" element={<Erreur />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
