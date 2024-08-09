08c9dfff691b4003904deca7b0c197abe88d5169
Priscouw <priscouw@gmail.com>
2024-08-09 18:59:59 +0200

    <feat> add new routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Accueil from "./pages/Accueil/Accueil";
 import Apropos from "./pages/Apropos/Apropos";
+import Logement from "./pages/Logement/Logement";
 import Erreur from "./pages/Erreur/Erreur";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Accueil />} />
           <Route path="/apropos" element={<Apropos />} />
+          <Route path="/logement" element={<Logement />} />
           <Route path="*" element={<Erreur />} />
         </Routes>
       </BrowserRouter>
