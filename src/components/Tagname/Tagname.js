6e900f4985b67fa8504ec25383866f9c0efd8d08
Priscouw <priscouw@gmail.com>
2024-08-10 18:00:13 +0200

    <feat> create tagname component

---
+++ src/components/Tagname/Tagname.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tagname = () => {
+  return (
+    <div className="tagnameContainer">
+      <p className="tagname-text">Cozy</p>
+    </div>
+  );
+};
+
+export default Tagname;
