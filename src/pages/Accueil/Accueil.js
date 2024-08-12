98ab241b60e4d7a3bd1b6427654cf9402ff19521
Priscouw <priscouw@gmail.com>
2024-08-12 13:29:41 +0200

    <feat> add card container

--- src/pages/Accueil/Accueil.js
+++ src/pages/Accueil/Accueil.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Banner/Banner";
-// import Card from "../../components/Card/Card";
+import Card from "../../components/Card/Card";
 
 import imgBannerAccueil from "../../assets/img/bannerHomePage.png";
 
@@ -14,7 +14,12 @@ const Accueil = () => {
         imageUrl={imgBannerAccueil}
         title="Chez vous, partout et ailleurs"
       />
-      <div className="cardContainer"></div>
+      <div className="cardContainer">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
       <Footer />
     </div>
   );
