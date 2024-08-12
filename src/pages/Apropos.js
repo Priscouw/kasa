c94f1954e236ddb2838c8b69ed8a30402d6f1e17
Priscouw <priscouw@gmail.com>
2024-08-12 19:22:47 +0200

    <feat> add maplist for topbar page about

--- src/pages/Apropos.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Apropos = () => {
-  return <div></div>;
-};
-
-export default Apropos;
--- src/pages/Apropos/Apropos.js
+++ src/pages/Apropos/Apropos.js
@@ -3,8 +3,19 @@ import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
 import Topbar from "../../components/Topbar/Topbar";
 import Footer from "../../components/Footer/Footer";
+import { aProposList } from "../../data/aProposList";
 
 import imgBannerApropos from "../../assets/img/bannerAboutPage.png";
+
+function AproposElement() {
+  return (
+    <>
+      {aProposList.map(({ title, content }) => (
+        <Topbar key={title} topbarTitle={title} topbarContent={content} />
+      ))}
+    </>
+  );
+}
 
 const Apropos = () => {
   return (
@@ -12,22 +23,7 @@ const Apropos = () => {
       <Header />
       <Banner imageUrl={imgBannerApropos} title="" />
       <div className="topbarContainer">
-        <Topbar
-          topbarTitle="Fiabilité"
-          topbarContent="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
-        />
-        <Topbar
-          topbarTitle="Respect"
-          topbarContent="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-        />
-        <Topbar
-          topbarTitle="Service"
-          topbarContent="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-        />
-        <Topbar
-          topbarTitle="Sécurité"
-          topbarContent="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-        />
+        <AproposElement />
       </div>
 
       <Footer />
