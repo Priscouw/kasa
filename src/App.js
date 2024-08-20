2dcc9837dc55f38eb46056b7c00691ce3666ef3b
Priscouw <priscouw@gmail.com>
2024-08-20 17:07:28 +0200

    <feat> add header and footer on App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Accueil from "./pages/Accueil/Accueil";
 import Apropos from "./pages/Apropos/Apropos";
 import Logement from "./pages/Logement/Logement";
@@ -9,12 +11,14 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Accueil />} />
           <Route path="/apropos" element={<Apropos />} />
           <Route path="/logement/:id" element={<Logement />} />
           <Route path="*" element={<Erreur />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
